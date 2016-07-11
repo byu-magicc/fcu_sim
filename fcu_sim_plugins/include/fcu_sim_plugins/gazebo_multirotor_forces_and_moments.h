d8cee8e9623e71dba1c9fec7fafcd56de39bcecb
James Jackson <superjax08@gmail.com>
2016-07-11 13:11:56 -0600

    added simple multirotor dynamics

---
+++ fcu_sim_plugins/include/fcu_sim_plugins/gazebo_multirotor_forces_and_moments.h
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef fcu_sim_PLUGINS_MULTIROTOR_FORCES_AND_MOMENTS_H
+#define fcu_sim_PLUGINS_MULTIROTOR_FORCES_AND_MOMENTS_H
+
+#include <stdio.h>
+
+#include <vector>
+#include <boost/bind.hpp>
+#include <Eigen/Eigen>
+
+#include <gazebo/common/common.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <ros/callback_queue.h>
+#include <ros/ros.h>
+
+#include <fcu_common/ExtendedCommand.h>
+#include <fcu_common/simple_pid.h>
+#include <std_msgs/Float32.h>
+#include <geometry_msgs/Vector3.h>
+
+#include "fcu_sim_plugins/common.h"
+
+namespace gazebo {
+static const std::string kDefaultWindSpeedSubTopic = "gazebo/wind_speed";
+
+
+class GazeboMultiRotorForcesAndMoments : public ModelPlugin {
+ public:
+  GazeboMultiRotorForcesAndMoments();
+
+  ~GazeboMultiRotorForcesAndMoments();
+
+  void InitializeParams();
+  void SendForces();
+
+ protected:
+  void UpdateForcesAndMoments();
+  void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+  void OnUpdate(const common::UpdateInfo & /*_info*/);
+
+ private:
+  std::string command_topic_;
+  std::string wind_speed_topic_;
+  std::string joint_name_;
+  std::string link_name_;
+  std::string parent_frame_id_;
+  std::string motor_speed_pub_topic_;
+  std::string namespace_;
+
+  physics::WorldPtr world_;
+  physics::ModelPtr model_;
+  physics::LinkPtr link_;
+  physics::JointPtr joint_;
+  physics::EntityPtr parent_link_;
+  event::ConnectionPtr updateConnection_; // Pointer to the update event connection.
+
+  // physical parameters
+  double mass_;
+  double Jx_;
+  double Jy_;
+  double Jz_;
+  double Jxz_;
+  double Jxy_;
+  double Jyz_;
+  double mu_; // drag coefficient (approx 0.1)
+
+  // Container for an Actuator
+  struct Actuator{
+    double max;
+    double tau_up;
+    double tau_down;
+  };
+
+  bool stabilized_; // flag of whether to use an inner PID controller for states
+
+  // Struct of Actuators
+  // This organizes the physical limitations of the abstract torques and Force
+  struct Actuators{
+    Actuator l;
+    Actuator m;
+    Actuator n;
+    Actuator F;
+  } actuators_;
+
+  // wind
+  struct Wind{
+    double N;
+    double E;
+    double D;
+  } wind_;
+
+  // container for forces
+  struct ForcesAndTorques{
+    double Fx;
+    double Fy;
+    double Fz;
+    double l;
+    double m;
+    double n;
+  } applied_forces_, actual_forces_, desired_forces_;
+
+  // container for PID controller
+  fcu_common::SimplePID roll_controller_;
+  fcu_common::SimplePID pitch_controller_;
+  fcu_common::SimplePID yaw_controller_;
+  fcu_common::SimplePID alt_controller_;
+
+  fcu_common::ExtendedCommand command_;
+
+  // Time Counters
+  double sampling_time_;
+  double prev_sim_time_;
+  double prev_control_time_;
+
+  ros::NodeHandle* node_handle_;
+  ros::Subscriber command_sub_;
+  ros::Subscriber wind_speed_sub_;
+
+  boost::thread callback_queue_thread_;
+  void QueueThread();
+  void WindSpeedCallback(const geometry_msgs::Vector3& wind);
+  void CommandCallback(const fcu_common::ExtendedCommand msg);
+  void ComputeControl(void);
+  double sat(double x, double max, double min);
+
+  std::unique_ptr<FirstOrderFilter<double>>  rotor_velocity_filter_;
+  math::Vector3 wind_speed_W_;
+};
+}
+
+#endif // fcu_sim_PLUGINS_MULTIROTOR_FORCES_AND_MOMENTS_H
